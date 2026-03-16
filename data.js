e2ea99ca90be8375abe3b650a7831878fd0bd5f3
salauddin <salauddin@hasan.com>
2026-03-16 22:58:54 +0600

    practice: solve JavaScript data type problems

---
+++ data.js
@@ -0,0 +1,127 @@
+// let name = "Rahim";        // String
+// let age = 21;              // Number
+// let isStudent = true;      // Boolean
+// let phone = ;          null// Null (ইচ্ছা করে খালি)
+// let address;               // Undefined (কিছু দেওয়া হয়নি)
+// let marks = [90, 80, 70];  // Array
+// let person = {name: "X"};  // Object
+
+
+
+// **Problem 1**
+// ```
+// নিচের প্রতিটার data type কী?
+
+// let a = "Hello";
+// let b = 42;
+// let c = true;
+// let d = null;
+// let e;
+// ```
+
+// let a = 'hello'
+// console.log(typeof a)
+
+// let b = 42;
+// console.log(typeof b)
+
+// let c = true;
+// console.log(typeof c);
+
+// let d = null;
+// console.log(typeof d);
+
+// let e;
+// console.log(typeof e)
+
+
+// **Problem 2**
+// ```
+// typeof দিয়ে data type বের করো:
+
+// let x = "Programming";
+// let y = 100;
+// let z = false;
+
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(typeof z);
+
+// Output কী হবে?
+// ```
+
+// let x = "Programming";
+// console.log(typeof x)
+
+// let y = 100;
+// console.log(typeof y)
+
+// let z = false;
+// console.log(typeof z)
+
+
+
+// **Problem 3**
+// ```
+// নিচেরটা দেখো এবং বলো কোনটা কোন type:
+
+// let isLoggedIn = false;
+// let userName = "Salauddin";
+// let userAge = 21;
+// let userPhone = null;
+// let userScore;
+// ```
+
+
+// let isLoggedIn = false;
+// console.log(typeof isLoggedIn);
+
+// let userName = "Salauddin";
+// console.log(typeof userName);
+
+// let userAge = 21;
+// console.log(typeof userAge);
+
+// let userPhone = null;
+// console.log(typeof userPhone)
+
+// let userScore;
+// console.log(typeof userScore)
+
+
+
+// **Problem 4**
+// ```
+// একজন student-এর info store করো:
+// - নাম (text)
+// - বয়স (number)
+// - পাস করেছে কিনা (true/false)
+// - ফোন নম্বর এখনো নেই
+
+// সঠিক data type দিয়ে variable বানাও।
+// ```
+
+
+// const name = 'salauddin';
+// let age = 21;
+// let pass = true;
+// let number;
+
+
+// **Problem 5**
+// ```
+// এই দুটোর মধ্যে পার্থক্য কী?
+
+// let a = null;
+// let b;
+
+// console.log(a);
+// console.log(b);
+
+let a = null;
+console.log(a)
+// nal
+
+let b;
+console.log(b);
+// undefined
