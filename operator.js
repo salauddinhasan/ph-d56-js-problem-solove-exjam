20a4f24e001325c9066d879ea6f4b97de950943f
salauddin <salauddin@hasan.com>
2026-03-16 23:37:28 +0600

    feat: practice arithmetic, comparison and logical operators in JavaScript

---
+++ operator.js
@@ -0,0 +1,78 @@
+
+// **Problem 1**
+// ```
+// let a = 15;
+// let b = 4;
+
+// এই দুটো দিয়ে যোগ, বিয়োগ, 
+// গুণ, ভাগ, আর ভাগশেষ বের করো।
+// ```
+
+// let a = 15;
+// let b = 4;
+
+// console.log('add', a + b)
+// console.log('sub', a - b)
+// console.log('mul', a * b)
+// console.log('vag', a / b)
+// console.log('vagSes', a % b)
+
+
+// **Problem 2**
+// ```
+// নিচের output কী হবে?
+
+// console.log(10 === "10"); 
+// console.log(10 == "10"); 
+// console.log(10 !== 5);  
+
+//   output : false
+//   output : true
+//   output : true
+
+
+// **Problem 3**
+// ```
+// let age = 20;
+
+// বলো age কি 18-র বেশি 
+// এবং 30-র কম?
+
+// (&&  ব্যবহার করো)
+
+// let age = 20;
+
+// if(age > 18 && age < 30) {
+//     console.log(age)
+// }
+
+
+// **Problem 4**
+// ```
+// let score = 10;
+// score += 5;
+// score *= 2;
+// console.log(score);
+
+// Output কী হবে?
+// ```
+
+
+// let score = 10;
+// score += 5;
+// score *= 2;
+// console.log(score);
+
+// output: 30
+
+
+
+// **Problem 5**
+// ```
+// let isLoggedIn = false;
+// console.log(!isLoggedIn);
+
+// Output কী হবে এবং কেন?
+
+// output: true;
+// karon: ! change hoye jabe flase ache true hobe 
